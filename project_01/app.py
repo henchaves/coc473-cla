84c0609b3f2830db531774135882ddf2f81e6364
Henrique Chaves <h.chaves@rioanalytics.com.br>
2021-08-18 22:40:44 -0300

    Implement Cholesky Decomposition

--- project_01/app.py
+++ project_01/app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import numpy as np
 import base64
+import pandas as pd
+
+from utils import (
+  decomposicao_lu,
+  resolve_lu,
+  decomposicao_cholesky,
+  resolve_cholesky
+)
 
 
 def download_txt(filename, label=""):
@@ -10,13 +18,32 @@ def download_txt(filename, label=""):
   href = f'<a href="data:file/csv;base64,{b64}" download="{filename}" target="_blank">{label}</a>'
   return href
 
-def read_matrix():
-  pass
+def convert_matrix_to_latex(np_array, label):
+  m, n = np_array.shape
+  latex_string = ""
+  latex_string += str(label)
+  latex_string += " = \n"
+  latex_string += "\\begin{bmatrix}"
+  for i in range(m):
+    row_string = ""
+    for j in range(n):
+      row_string += str(np_array[i][j])
+      if j != n-1:
+        row_string += " & "
+      else:
+        row_string += " \\\\ "
+    latex_string += row_string
+  latex_string += "\\end{bmatrix}"
+  return latex_string
+
+def read_matrix(file_input):
+  return pd.read_table(file_input, header=None, sep=" ").to_numpy()
 
 def main():
   st.title("COC473 - Primeiro trabalho prático")
   # st.header("UFRJ - Escola Politécnica - Eng. de Computação e Informação")
-  menu = ["Decomposição LU", "Tarefa 2", "Tarefa 3"]
+  menu = ["Decomposição LU", "Decomposição de Cholesky", 
+          "Tarefa 3"]
   choice = st.sidebar.selectbox("Implementações", menu)
 
   if choice == "Decomposição LU":
@@ -24,35 +51,65 @@ def main():
 
     st.markdown("<p>Forneça os arquivos da <strong>Matriz A</strong> e <strong>Vetor B</strong>, em arquivos no formato <strong>txt</strong>. Separe as colunas utilizando um espaço, conforme os exemplos:</p>", unsafe_allow_html=True)
 
-    st.markdown(download_txt("src/matrix_A_sample.txt", label="Download exemplo de Matriz A"), unsafe_allow_html=True)
+    st.markdown(download_txt("src/matrix_A_lu_sample.txt", label="Download exemplo de Matriz A"), unsafe_allow_html=True)
 
-    st.markdown(download_txt("src/vector_B_sample.txt", label="Download exemplo do Vetor B"), unsafe_allow_html=True)
+    st.markdown(download_txt("src/vector_B_lu_sample.txt", label="Download exemplo do Vetor B"), unsafe_allow_html=True)
 
     st.markdown("<p>Faça os uploads abaixo:</p>", unsafe_allow_html=True)
 
-    matrix_A = st.file_uploader("Matriz A", type="txt")
-    if matrix_A:
-      st.latex(r"""
-      Matriz A = 
-      \begin{bmatrix}
-      1 & 2 & 2 \\
-      4 & 4 & 2 \\
-      4 & 6 & 4\\
-     \end{bmatrix}
-      """)
-    vector_B = st.file_uploader("Vetor B", type="txt")
-    if vector_B:
-      st.latex(r"""
-      Vetor B = 
-      \begin{bmatrix}
-      3 \\
-      6 \\
-      10 \\
-     \end{bmatrix}
-      """)
+    matrix_A_input = st.file_uploader("Matriz A", type="txt")
+    if matrix_A_input:
+      matrix_A = read_matrix(matrix_A_input)
+      st.latex(convert_matrix_to_latex(matrix_A, "Matriz A"))
 
-  elif choice == "Tarefa 2":
-    st.header("Implementação 2 - Tarefa 2")
+    vector_B_input = st.file_uploader("Vetor B", type="txt")
+    if vector_B_input:
+      vector_B = read_matrix(vector_B_input)
+      st.latex(convert_matrix_to_latex(vector_B, "Vetor B"))
+    
+    if (matrix_A_input and vector_B_input):
+      button = st.button("Calcular")
+      if button:
+        try:
+          matrix_L, matrix_U = decomposicao_lu(matrix_A)
+          vector_X = resolve_lu(matrix_L, matrix_U, vector_B)
+          st.latex(convert_matrix_to_latex(matrix_L, "Matriz L"))
+          st.latex(convert_matrix_to_latex(matrix_U, "Matriz U"))
+          st.latex(convert_matrix_to_latex(vector_X, "Vetor X"))
+        except:
+          st.error("A combinação de vetor B e matriz A fornecida não tem solução.")
+
+  elif choice == "Decomposição de Cholesky":
+    st.subheader("Implementação 1.2 - Decomposição de Cholesky")
+
+    st.markdown("<p>Forneça os arquivos da <strong>Matriz A</strong> e <strong>Vetor B</strong>, em arquivos no formato <strong>txt</strong>. Separe as colunas utilizando um espaço, conforme os exemplos:</p>", unsafe_allow_html=True)
+
+    st.markdown(download_txt("src/matrix_A_cholesky_sample.txt", label="Download exemplo de Matriz A"), unsafe_allow_html=True)
+
+    st.markdown(download_txt("src/vector_B_cholesky_sample.txt", label="Download exemplo do Vetor B"), unsafe_allow_html=True)
+
+    st.markdown("<p>Faça os uploads abaixo:</p>", unsafe_allow_html=True)
+
+    matrix_A_input = st.file_uploader("Matriz A", type="txt")
+    if matrix_A_input:
+      matrix_A = read_matrix(matrix_A_input)
+      st.latex(convert_matrix_to_latex(matrix_A, "Matriz A"))
+
+    vector_B_input = st.file_uploader("Vetor B", type="txt")
+    if vector_B_input:
+      vector_B = read_matrix(vector_B_input)
+      st.latex(convert_matrix_to_latex(vector_B, "Vetor B"))
+    
+    if (matrix_A_input and vector_B_input):
+      button = st.button("Calcular")
+      if button:
+        try:
+          matrix_L = decomposicao_cholesky(matrix_A)
+          vector_X = resolve_cholesky(matrix_L, vector_B)
+          st.latex(convert_matrix_to_latex(matrix_L, "Matriz L"))
+          st.latex(convert_matrix_to_latex(vector_X, "Vetor X"))
+        except:
+          st.error("A combinação de vetor B e matriz A fornecida não tem solução.")
   
   elif choice == "Tarefa 3":
     st.header("Implementação 3 - Tarefa 3")
